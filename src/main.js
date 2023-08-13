9772276f9be2ef7c88c3ffda9f6a3a677a3c77b5
Никита <belokovilskij@yandex.ru>
2023-08-13 13:49:38 +0700

    Время разбивать камни

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import FilterView from './view/filter-view.js';
+import InfoView from './view/info-view.js';
+import { render } from './render.js';
+import EventsPresenter from './presenter/events-presenter.js';
+
+const tripFiltersElement = document.querySelector('.trip-events');
+const tripEventsFiltersElement = document.querySelector('.trip-controls__filters');
+const tripMainElement = document.querySelector('.trip-main');
+const eventsPresenter = new EventsPresenter({eventsContainer: tripFiltersElement});
+
+render(new InfoView(), tripMainElement, 'afterbegin');
+render(new FilterView(), tripEventsFiltersElement);
+
+eventsPresenter.init();
